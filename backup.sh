06514119fb162e2bde8ff342fbe29c5ed926dd5e
AchrosEsson <47488896+AchrosEsson@users.noreply.github.com>
2021-02-17 17:22:44 +0100

    Delete backup.sh

--- backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#### Bash-Script zum Konfigureieren eines Backup ####
-
-echo -n 'Enter your desired Backup destination: ' 
-        read backup-location
-        echo "rsync -zrvh /home $backup-location" >> ~/.zshrc
-
-exit
